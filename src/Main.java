b96ea5ab83c127bf57016a9626712ef5f7c966d0
mv235 <izmailoff.ehduard@yandex.ru>
2024-11-08 23:31:46 +0300

    HW-1

---
+++ src/Main.java
@@ -0,0 +1,62 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+
+        var dog = 0.8;
+        dog = dog + 4;
+        dog = dog - 3.5;
+        System.out.println(dog);
+        var cat = 3.6;
+        cat = cat + 4;
+        cat = cat - 1.6;
+        System.out.println(cat);
+        var paper = 763789;
+        paper = paper + 4;
+        paper = paper - 7639;
+        System.out.println(paper);
+        //задание №1,2,3.
+
+        var friend = 19;
+        friend = friend + 2;
+        friend = friend / 7;
+        System.out.println(friend);
+        //задание №4.
+
+        var frog = 3.5;
+        System.out.println(frog);
+        frog = frog * 10;
+        System.out.println(frog);
+        frog = frog / 3.5;
+        System.out.println(frog);
+        frog = frog + 4;
+        System.out.println(frog);
+        //задание №5.
+
+        var BoxerWeight1 = 78.2;
+        var BoxerWeight2 = 82.7;
+        System.out.println(BoxerWeight1 + BoxerWeight2);
+        var TotalWeight = 160.9;
+        System.out.println(TotalWeight);
+        System.out.println(BoxerWeight2 - BoxerWeight1);
+        // задание №6
+
+        var BoxerWeight3 = 78.2;
+        var BoxerWeight4 = 82.7;
+        System.out.println(BoxerWeight4%BoxerWeight3);
+        // задание №7
+
+        var TotalTime = 640;
+        System.out.println(TotalTime);
+        var OpeningTime = 8;
+        System.out.println(OpeningTime);
+        System.out.println(TotalTime/OpeningTime);
+        // задание №8.1
+
+        var TotalEmployees = 80+94;
+        System.out.println(TotalEmployees);
+        var TotalHours = 174*8;
+        System.out.println(TotalHours);
+        System.out.println("Если в компании работает " + TotalEmployees + " человека " + "то всего " + TotalHours + " часа работы может быть поделено между сотрудниками ");
+        // задание №8.2.2.
+    }
+}
